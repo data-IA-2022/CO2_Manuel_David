0aecec88a0c3c8c321a0bde3664d2bdf380acc91
Dakoro <david.korobetski@gmail.com>
2023-06-06 23:04:49 +0200

    load data + primary key

--- db_load.py
+++ db_load.py
@@ -1,22 +1,86 @@
 import pandas as pd
-from sqlalchemy import create_engine
-from yaml import safe_load
+from utils import get_api_df, format_df, get_engine, add_primary_key
+from sqlalchemy import ( 
+    Integer, 
+    String,  
+    Float, 
+    Boolean, 
+    SmallInteger,
+    Text,
+    text
+)
 
-df = pd.read_csv('data/2016_Building_Energy_Benchmarking.csv')
+def main():
+    
+    engine = get_engine(echo_arg=True)
 
-yml_file = safe_load(open('config.yml'))
+    df = format_df(get_api_df())
+        
+    types = {
+        'OSEBuildingID': String(length=50226),
+        'DataYear': SmallInteger,
+        'BuildingType': String(length=20),
+        'PrimaryPropertyType': String(length=27),
+        'PropertyName': String(length=72),
+        'Address': String(length=41),
+        'City': String(length=7),
+        'State': String(length=2),
+        'ZipCode': Float,
+        'TaxParcelIdentificationNumber': String(length=25),
+        'CouncilDistrictCode': SmallInteger,
+        'Neighborhood': String(length=22),
+        'Latitude': Float,
+        'Longitude': Float,
+        'YearBuilt': SmallInteger,
+        'NumberofBuildings': SmallInteger,
+        'NumberofFloors': SmallInteger,
+        'PropertyGFATotal': Integer,
+        'PropertyGFAParking': Integer,
+        'PropertyGFABuildings': Integer,
+        'ListOfAllPropertyUseTypes': String(length=255),
+        'LargestPropertyUseType': String(length=52),
+        'LargestPropertyUseTypeGFA': Float,
+        'SecondLargestPropertyUseType': String(length=52),
+        'SecondLargestPropertyUseTypeGFA': Float,
+        'ThirdLargestPropertyUseType': String(length=52),
+        'ThirdLargestPropertyUseTypeGFA': Float,
+        'YearsENERGYSTARCertified': String(length=60),
+        'ENERGYSTARScore': SmallInteger,
+        'SiteEUI': Float,
+        'SiteEUIWN': Float,
+        'SourceEUI': Float,
+        'SourceEUIWN': Float,
+        'SiteEnergyUse': Float,
+        'SiteEnergyUseWN': Float,
+        'SteamUse': Float,
+        'Electricity_kWh': Float,
+        'Electricity_kBtu': Float,
+        'NaturalGas_therms': Float,
+        'NaturalGas_kBtu': Float,
+        'DefaultData': Boolean,
+        'Comments': Text,
+        'ComplianceStatus': String(length=28),
+        'Outlier': String(length=12),
+        'TotalGHGEmissions': Float,
+        'GHGEmissionsIntensity': Float
+    }
 
-config = yml_file['co2']
-
-host = config['host']
-user = config['user']
-password = config['password']
-database = config['database']
-
-
-url = f'postgresql+psycopg2://{user}:{password}@{host}/{database}'
-print(url)
-
-engine = create_engine(url)
-print(engine)
+    df.to_sql(
+        'buildings', 
+        engine, method='multi', 
+        if_exists="replace", 
+        index=False,
+        schema='public',
+        dtype=types
+        )
+    
+    print('The database has been loaded')
+    
+    with engine.connect() as conn:
+        conn.execute(text("""ALTER TABLE buildings ADD PRIMARY KEY("OSEBuildingID");"""))
+        conn.commit()
+    print('Primary key has been added')
+    
+if __name__=='__main__':
+    main()
 
